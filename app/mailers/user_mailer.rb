90e6fcc2de31cebb0883f5bb366a9f25fa4dbc7d
Darnell Gillead <dlgillead@gmail.com>
2017-07-24 16:29:49 -0500

    added styling for home page

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'test@test.com'
-
-  def confirm_account_deletion(user)
-    @user = user
-    mail(to: @user.email, subject: 'Account deletion confirmation')
-  end
-end
